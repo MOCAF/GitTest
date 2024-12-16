693c70586b29ad456131ad16cd827b62ac7e7fc8
MOCAF <hayeon9956@naver.com>
2024-12-16 18:15:04 +0900

    2024.12.16(월) commit

---
+++ app/src/main/java/com/cookandroid/linc_sadaju/ForCustomer.java
@@ -0,0 +1,90 @@
+package com.cookandroid.linc_sadaju;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ForCustomer extends AppCompatActivity {
+    Button question, report;
+    ExpandableListView expandableListView;
+    List<String> listGroup;
+    Map<String, List<String>> listItem;
+    Mypage_FAQAdapter adapter;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage_for_customer);
+
+        question = findViewById(R.id.question);
+        report = findViewById(R.id.report);
+
+        expandableListView = findViewById(R.id.expandableListView);
+        listGroup = new ArrayList<>();
+        listItem = new HashMap<>();
+        adapter = new Mypage_FAQAdapter((Context) this, listGroup, (HashMap<String, List<String>>) listItem);
+        expandableListView.setAdapter(adapter);
+        initListData();
+
+        // Question 버튼 클릭 리스너
+        question.setOnClickListener(view -> {
+            Intent intent = new Intent(ForCustomer.this, Mypage_OnebyoneInquiry.class);
+            startActivity(intent);
+        });
+
+        // Report 버튼 클릭 리스너
+        report.setOnClickListener(view -> {
+            EditText inputReport = new EditText(ForCustomer.this);
+
+            AlertDialog.Builder builderRpt = new AlertDialog.Builder(ForCustomer.this);
+            builderRpt.setTitle("신고 내용을 작성해 보세요.");
+            builderRpt.setView(inputReport);
+            builderRpt.setPositiveButton("OK", (dialog, which) -> {
+                String userInput = inputReport.getText().toString();
+                Toast.makeText(ForCustomer.this, "당신의 신고를 확인했습니다! ", Toast.LENGTH_SHORT).show();
+            });
+
+            builderRpt.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+            builderRpt.show();
+        });
+    }
+
+    private void initListData() {
+        listGroup.clear();
+        listItem.clear();
+
+        listGroup.add("문제 1");
+        listGroup.add("문제 2");
+        listGroup.add("문제 3");
+
+
+        List<String> problem1 = new ArrayList<>();
+        problem1.add("답1");
+
+        List<String> problem2 = new ArrayList<>();
+        problem2.add("답2");
+
+        List<String> problem3 = new ArrayList<>();
+        problem3.add("답3");
+
+        listItem.put(listGroup.get(0), problem1);
+        listItem.put(listGroup.get(1), problem2);
+        listItem.put(listGroup.get(2), problem3);
+
+        adapter.notifyDataSetChanged();  // 데이터 변경을 어댑터에 알림
+    }
+}
---
+++ app/src/main/java/com/cookandroid/linc_sadaju/MyPage.java
@@ -0,0 +1,56 @@
+package com.cookandroid.linc_sadaju;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPage extends AppCompatActivity {
+
+    Button editor, product, writing, list, service, logout, deleteUser;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_mypage);
+
+        // intent 창 연계 버튼
+        editor = findViewById(R.id.editor);         // 프로필 편집 버튼
+        product = findViewById(R.id.recentSeenProduct_button);   // 최근 본 상품 버튼
+        writing = findViewById(R.id.writing_button);       // 내가 작성한 글 버튼
+        list = findViewById(R.id.exchangeList);   // 거래 목록 버튼
+        service = findViewById(R.id.service);       // 고객센터 버튼
+        logout = findViewById(R.id.logout);         // 로그아웃 버튼
+        deleteUser = findViewById(R.id.deleteUser); // 회원탈퇴
+
+        // 거래 목록
+        list.setOnClickListener(v -> {
+            Intent intent = new Intent(MyPage.this, Mypage_Trade.class);
+            startActivity(intent);
+        });
+
+        // 고객 센터
+        service.setOnClickListener(v -> {
+            Intent intent = new Intent(MyPage.this, ForCustomer.class);
+            startActivity(intent);
+        });
+
+        // 설정 편집
+        editor.setOnClickListener(v -> {
+            Intent intent = new Intent(MyPage.this, Mypage_Editor.class);
+            startActivity(intent);
+        });
+
+        // 최근 본 목록
+        product.setOnClickListener(v -> {
+            Intent intent = new Intent(MyPage.this, Mypage_RecentSeenProduct.class);
+            startActivity(intent);
+        });
+
+    }
+}
